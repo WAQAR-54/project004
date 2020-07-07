340cab1633be0809fa2c3e71368cb50af3e0fafa
WAQAR-54 <56077768+WAQAR-54@users.noreply.github.com>
2020-07-07 10:59:45 +0500

    initial commit

---
+++ waq/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import logo from './logo.svg';
+import Room from './Room';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div>
+<Room>yarn </Room>
+    </div>
+  );
+}
+
+export default App;
---
+++ waq/src/Room.js
@@ -0,0 +1,20 @@
+import React ,{useState} from 'react';
+// //import logo from './logo.svg';
+// import Room from './Room';
+// import './App.css';
+
+function Room() {
+  const[islite ,setlite]= useState(true);
+  
+  return (
+
+    <div>
+      This is room Component is  {islite? "lit": "dark" } 
+      <br/>
+      My Name is Waqar Azeem  
+
+    </div>
+  );
+}
+
+export default Room;
